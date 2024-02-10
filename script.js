6257d916988469db040ff8610950d90e16372f26
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-11 01:04:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // complete this js code
-function Person(name, age) {}
+class Person{
+	constructor(name, age) {
+		this._name=name;
+		this._age=age;
+	}
+	greet(){
+		console.log(`Hello, my name is ${this._name}, I am ${this.age} years old.`)
+	}
+} 
 
-function Employee(name, age, jobTitle) {}
+class Employee extends Person  {
+	constructor(name, age, jobTitle){
+		super(name,age);
+		this._jobTitle=jobTitle;
+	}
+	jobgreet(){
+		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`)
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
