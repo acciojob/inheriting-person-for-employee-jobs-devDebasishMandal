93b56759c882331eb28bf79793c9c302fd3887e9
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-11 02:03:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -17,8 +17,10 @@ class Employee extends Person  {
 		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
 	}
 }
-const person = new Person("Alice", 25); 
-const employee = new Employee("Bob", 30, "Manager");
+// Hello, my name is Alice and I am 25 years old.
+const person = new Employee("Bob", 30, "Manager");
+const employee = new Person("Alice", 25); 
+
 
 // Do not change code below this line
 window.Person = Person;
