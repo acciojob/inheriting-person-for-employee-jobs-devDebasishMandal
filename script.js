db9668cfb0a5a84d983e76ae321b50c14b32f9d3
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-02-11 12:19:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 class Person{
 	constructor(name, age) {
-		this._name=name;
-		this._age=age;
+		this.name=name;
+		this.age=age;
 	}
 	greet(){
-		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old.`);
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 	}
 } 
 
 class Employee extends Person  {
 	constructor(name,age,jobTitle){
 		super(name,age);
-		this._jobTitle=jobTitle;
+		this.jobTitle=jobTitle;
 	}
 	jobGreet(){
-		console.log(`Hello, my name is ${this._name}, I am ${this._age} years old, and my job title is ${this._jobTitle}.`);
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this._jobTitle}.`);
 	}
 }
 // Hello, my name is Alice and I am 25 years old.
